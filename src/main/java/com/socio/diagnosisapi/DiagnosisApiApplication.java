7f51edd268358bc61d014d8ac3fa33471c904633
Sebastian Rios <sebasri92@gmail.com>
2026-03-19 00:14:34 -0500

    Mi primer commit

---
+++ src/main/java/com/socio/diagnosisapi/DiagnosisApiApplication.java
@@ -0,0 +1,13 @@
+package com.socio.diagnosisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socio/diagnosisapi/DiagnosisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socio.diagnosisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
